a800d4f25f544bea4a94f37b68e8c4a41bd1ac65
泰斗 横山 <13.taito@gmail.com>
2022-12-19 13:26:35 +0900

    追加変更

--- TrainingProgram02/App.vb
+++ TrainingProgram02/App.vb
@@ -31,30 +31,22 @@ Public Class App
     'キーの存在確認
     Public Shared Sub ConfigCheck()
 
-        'すべてのキーとその値を取得して存在を確認
-        Dim key As String
-        For Each key In System.Configuration.ConfigurationManager.AppSettings.AllKeys
+        '取得するキー名を配列で配置
+        Dim KeyNameArry As String() = {"filePath", "bk_DirPath", "outputPath"}
 
-            For Each Keys As String In KeyNameArry
+        For Each Keys As String In KeyNameArry
 
-                If Array.IndexOf(System.Configuration.ConfigurationManager.AppSettings.AllKeys, Keys) = -1 Then
+            If Array.IndexOf(System.Configuration.ConfigurationManager.AppSettings.AllKeys, Keys) = -1 Then
 
-                    MessageBox.Show("ファイルに指定されたキーが存在しません" & vbLf & "configファイルの内容を確認してください" & vbLf & "キー：" & Keys)
+                MessageBox.Show("ファイルに指定されたキーが存在しません" & vbLf & "configファイルの内容を確認してください" & vbLf & "キー：" & Keys)
 
-                End If
-
-            Next
-
-            If System.Configuration.ConfigurationManager.AppSettings.AllKeys.Length > KeyNameArry.Length Then
-                MessageBox.Show("ファイルに指定されたキーが指定数より多いです" & vbLf & "configファイルの内容を確認してください")
             End If
 
-            'フォルダの存在を確認
-            Dim FolderName As String = ConfigurationManager.AppSettings("filePath")
-            If Not System.IO.Directory.Exists(System.Configuration.ConfigurationManager.AppSettings(key)) Then
-                MessageBox.Show("ファイルに指定されたキーが存在しません" & vbLf & "configファイルの内容を確認してください" & vbLf & "キー：" & key)
-            End If
-        Next key
+        Next
+
+        If System.Configuration.ConfigurationManager.AppSettings.AllKeys.Length > KeyNameArry.Length Then
+            MessageBox.Show("ファイルに指定されたキーが指定数より多いです" & vbLf & "configファイルの内容を確認してください")
+        End If
 
         'フォルダの存在を確認
         Dim FolderName As String = ConfigurationManager.AppSettings("filePath")
@@ -62,7 +54,7 @@ Public Class App
             MessageBox.Show("'" + FolderName + "'は存在します。")
         Else
             MessageBox.Show("configファイルに指定されたフォルダが存在しません。" & vbLf & "configファイルを確認してください" & vbLf & "パス：" & FolderName)
-            End If
+        End If
 
     End Sub
 
